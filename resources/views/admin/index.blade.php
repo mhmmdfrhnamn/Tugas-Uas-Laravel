a288d005e86a82f7ad18dd451aec84db27df84cf
Muhammad Farhan Amien <farhanexcellentz17@gmail.com>
2025-01-14 02:12:02 +0700

    membuat folder admin dan layouts di resources/views serta membuat file didalam folder tersebut dan menulis beberapa sintaks yang dibutuhkan

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+@extend('layouts.content')
+@section('main-content')
+<div class="container">
+    <h2>
+        Unggah Foto 
+    </h2>
+</div>
+@endsection
---
+++ resources/views/layouts/content.blade.php
@@ -0,0 +1,10 @@
+@include('layouts.header')
+
+<body>
+    @yield('main-content')
+    @include('layouts.footer')
+    @stack('js')
+
+</body>
+
+</html>
